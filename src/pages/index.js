a687af25f57fdf48935363eb7268368a4fd30eb6
devbysalas <rgkalin@gmail.com>
2018-10-04 19:19:11 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi, I'm Carlos Alfaro</h1>
+    <p>Web developer</p>
+  </Layout>
+)
+
+export default IndexPage
